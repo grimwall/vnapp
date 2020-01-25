6d288af7a60b73aff6cb38761e99ec5107dad8e4
ahmet_apaydin <ahmeta@gmail.com>
2020-01-25 09:09:37 +0300

    hark!

---
+++ src/main/java/com/aveng/vnapp/VnappApplication.java
@@ -0,0 +1,13 @@
+package com.aveng.vnapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aveng/vnapp/VnappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aveng.vnapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
